66c63d658d0009ea18425b92fabf377e3492c93f
Xu Xing <xu.xing@outlook.com>
2020-04-29 22:33:04 +0800

    Rename

--- examples/add_image/add_image.cpp
+++ examples/add_image/add_image.cpp
@@ -15,32 +15,36 @@
 #include <string.h>
 #include <vector>
 
-#include "ComputeBufferToImageOp.h"
+#include "ComputeImageOp.h"
+
 
 #define DEBUG (!NDEBUG)
+
 
 #if defined(VK_USE_PLATFORM_ANDROID_KHR)
 void android_main(android_app *state) { android_realmain(state); }
 #else
+
 int main() {
-  typedef float DATA_TYPE;
   ComputeOp::InitParams params;
+
   std::vector<DATA_TYPE> computeInput(BUFFER_ELEMENTS);
   std::vector<DATA_TYPE> computeFilter(BUFFER_ELEMENTS);
   std::vector<DATA_TYPE> computeOutput(BUFFER_ELEMENTS);
   // Fill input data
   uint32_t n = 0;
-  std::generate(computeInput.begin(), computeInput.end(), [&n] { return 33+n++; });
+  uint32_t start = 0x3f800000;
+  std::generate(computeInput.begin(), computeInput.end(), [&n] { return 5.0+(DATA_TYPE)n++; });
 
   uint32_t m = 0;
   std::generate(computeFilter.begin(), computeFilter.end(),
-                [&m] { return 125+m++; });
+                [&m] {return 2.0+(float) m++;});
   params.computeInput = computeInput;
   params.computeFilter = computeFilter;
   params.computeOutput = computeOutput;
   params.shader_path = "shaders/add_image/add_image.comp.spv";
 
-  ComputeOp *computeOp = new ComputeBufferToImageOp(params);
+  ComputeOp *computeOp = new ComputeImageOp(params);
   computeOp->execute();
   computeOp->summary();
   delete (computeOp);
