0e00ada2759ba3c25515c45a8bde326d8e5e640b
Xu Xing <xu.xing@outlook.com>
2020-05-14 00:13:45 +0800

    Nit, copy_image

--- examples/copy_image/copy_image.cpp
+++ examples/copy_image/copy_image.cpp
@@ -19,16 +19,45 @@
 
 #define DEBUG (!NDEBUG)
 
-#define BUFFER_ELEMENTS 32
-
 #if defined(VK_USE_PLATFORM_ANDROID_KHR)
 void android_main(android_app *state) { android_realmain(state); }
 #else
 int main() {
+#if 0
+#define BUFFER_ELEMENTS 32
   ComputeOp::InitParams params;
   std::vector<DATA_TYPE> computeInput(BUFFER_ELEMENTS);
   std::vector<DATA_TYPE> computeFilter(BUFFER_ELEMENTS);
   std::vector<DATA_TYPE> computeOutput(BUFFER_ELEMENTS);
+#endif
+#if 1
+  // TODO: understand 4x8. When height is 1, ceil when height/2.
+  const int width = 32;
+  const int height = 1;
+  const int WORKGROUPSIZE_X = 1;
+  const int WORKGROUPSIZE_Y = 1;
+  const int WORKGROUPSIZE_Z = 1;
+
+  ComputeOp::InitParams params;
+  params.inputWidth = width;
+  params.inputHeight = height;
+  params.filterWidth = width;
+  params.filterHeight = height;
+  params.outputWidth = width;
+  params.outputHeight = height;
+  params.DISPATCH_X = ceil((float)width / WORKGROUPSIZE_X);
+  params.DISPATCH_Y = ceil((float)height / WORKGROUPSIZE_Y);
+  params.DISPATCH_Z = 1;
+  params.WORKGROUPSIZE_X = WORKGROUPSIZE_X;
+  params.WORKGROUPSIZE_Y = WORKGROUPSIZE_Y;
+  params.WORKGROUPSIZE_Z = WORKGROUPSIZE_Z;
+  int BUFFER_ELEMENTS = params.inputWidth * params.inputHeight;
+  std::vector<DATA_TYPE> computeInput(BUFFER_ELEMENTS);
+  BUFFER_ELEMENTS = params.filterWidth * params.filterHeight;
+  std::vector<DATA_TYPE> computeFilter(BUFFER_ELEMENTS);
+  BUFFER_ELEMENTS = params.outputWidth * params.outputHeight;
+  std::vector<DATA_TYPE> computeOutput(BUFFER_ELEMENTS);
+#endif
   // Fill input data
   uint32_t n = 0;
   std::generate(computeInput.begin(), computeInput.end(),
